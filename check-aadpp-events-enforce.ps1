32c1a69af19de60a64aca91f85ceb1bb45555518
Bard Holtbakk <holtbakk@gmail.com>
2022-10-24 15:04:50 +0200

    Update check-aadpp-events-enforce.ps1

--- check-aadpp-events-enforce.ps1
+++ check-aadpp-events-enforce.ps1
@@ -4,26 +4,28 @@
     
     .DESCRIPTION
         Check for detections by Azure Ad Password Protection agent, and add users to $ADgroup
-        Version 0.9
-        Need Microsoft-AzureADPasswordProtection-DCAgent installed on DCs
-        Need $ADGroup to keep track of detected users.
-        Need sufficient rights in AD
-S
+        Need Microsoft-AzureADPasswordProtection-DCAgent installed on domain controllers
+        Need an $ADGroup to keep track of detected users
+        Need sufficient rights in AD. Domain admin or delegated on Microsoft-AzureADPasswordProtection-DCAgent/Admin
+
     .PARAMETER Output
         $True to force output to screen
 
-    .PARAMETER Days
-        Number of days to evaluate from eventlog
+    .PARAMETER Hours
+        Number of hours to evaluate from eventlog
 
     .NOTES
-        Author: Bard Holtbakk
-        Todo: Use Compare-Object to update $ADGroup rather than -clear and repopulate
-        Todo: Use Try to fetch DCs by Get-ADComputer block
-        Todo: Log to file
+        Author: Bård Holtbakk
+        Version 1.1 - Swapped days for hours
+        Version 1.0 - Added logging to file
+        Version 0.9 - Added Try/Catch for domain controllers
 
     .EXAMPLE
-        Run the script and output to screen. Evaluate data from past 7 days
-        .\check-aadpp-events-enforce.ps1 -Output:$True -Days:7
+        Run the script and output to screen. Evaluate data from past 14 days
+        .\check-aadpp-events-enforce.ps1 -Output:$True -Hours:336
+
+        Run the script with no output. Evaluate data from past 12 hours
+        .\check-aadpp-events-enforce.ps1 -Hours:12
 #>
 
 [CmdletBinding()]
@@ -33,17 +35,21 @@ Param (
     [Bool]$Output = $False,
 
     [Parameter(Mandatory=$False)]
-    [ValidateRange(1,99)]
-    [Int]$Days = 3
+    [ValidateRange(1,8760)]
+    [Int]$Hours = 48
 )
 
 BEGIN {
+
     # Initiate timer
     $StartScript = Get-Date
     # Modify this to match your AD group name
     $ADGroup = "SG-Users-AzureAdPasswordProtection-Detected"
     # Modify this to retrieve all domain controllers
-    $DomainControllers = (Get-ADComputer -SearchBase "OU=Domain Controllers,DC=NMBU,DC=NO" -Filter *).Name
+    Try { $DomainControllers = (Get-ADComputer -SearchBase "OU=Domain Controllers,DC=NMBU,DC=NO" -Filter *).Name } Catch { Write-Host -ForegroundColor Red "Domain controllers unreachable!" ; break }
+    # Table of event ids to monitor
+    $EventIds = @('30009')
+
 }
 
 PROCESS {
@@ -53,8 +59,8 @@ PROCESS {
         Get-WinEvent -FilterHashtable @{
             ProviderName = 'Microsoft-AzureADPasswordProtection-DCAgent'
             LogName = 'Microsoft-AzureADPasswordProtection-DCAgent/Admin'
-            Id = 30008,30007,30010,30009,30029,30028,10024,10025
-            StartTime =  (Get-Date).AddDays(-$Days)
+            Id = $EventIds
+            StartTime =  (Get-Date).AddHours(-$Hours)
             EndTime = Get-Date
         } -ComputerName $_ -ErrorAction SilentlyContinue 
     } | Select-Object Id,TimeCreated,Message | Sort-Object TimeCreated
@@ -62,16 +68,16 @@ PROCESS {
     # Checking if DomainControllers and $ADGroup are accessable
     If (($DomainControllers) -and ($DomainControllers.Count -ge "1") -and (Get-ADGroup -Filter "Name -eq '$($ADGroup)'")) {
 
-        if ($Output) { Write-Host -ForegroundColor Yellow Fetching old users from group and evaluating logs for new events. Checking $Days days. }
+        if ($Output) { Write-Host -ForegroundColor Yellow Fetching old users from group and evaluating logs for new events. Checking $Hours hours. }
 
         # Add all users from $ADGroup to a hashtable
         $UsersFromGroup = foreach ($Member in (Get-ADGroupMember -Identity $($ADGroup))) {
-            New-Object PSCustomObject -Property @{ UserName = $Member.SamAccountName ; TimeCreated = (Get-Date).AddDays(-$Days) }
+            New-Object PSCustomObject -Property @{ UserName = $Member.SamAccountName ; TimeCreated = (Get-Date).AddHours(-$Hours) }
         }
 
-        # Add all users from WinEvent to a hashtable from the last $Days
-        $UsersFromEvents = foreach ($Event in $PasswordEvents | Where-object TimeCreated -gt (Get-Date).AddDays(-$Days)) {
-            If ($Event.Id -eq "30009") {
+        # Add all users from WinEvent to a hashtable from the last $Hours
+        $UsersFromEvents = foreach ($Event in $PasswordEvents | Where-object TimeCreated -gt (Get-Date).AddHours(-$Hours)) {
+            If ($Event.Id -in $EventIds) {
                 $ThisUserName = (($event.Message.Split("UserName: "))[1].Split("FullName: ")[0]).Trim()
                 $ThisTimeCreated = $Event.TimeCreated
                 New-Object PSCustomObject -Property @{ UserName = $ThisUserName ; TimeCreated = $ThisTimeCreated }
@@ -85,36 +91,40 @@ PROCESS {
         $InsecureUsers = $InsecureUsers | Group-Object UserName | ForEach-Object { $_.Group | Sort-Object TimeCreated | Select-Object -last 1 }
 
         # Loop through and check latest timestamp against PasswordLastSet in AD for each user Get-Aduser -Identity $User.UserName -Properties PasswordLastSet
-        $NoDuplciatesBackToGroup = foreach ($User in $InsecureUsers) {
-            If ($CheckUser = Get-ADUser -Filter "CN -eq '$($User.UserName)' -and Enabled -eq 'True'" -Properties PasswordLastSet) {
-                If ([datetime]($CheckUser).PasswordLastSet -gt [datetime](get-date -Date $User.TimeCreated)) {
-                    if ($Output) { write-host -ForegroundColor Green "User HAS changed password after latest risk detection => $($User.UserName)"  }
-                    # Log - Remove user from Group.
-                }
+        ForEach ($User in $InsecureUsers) {
+            If ($CheckUser = Get-ADUser -Filter "CN -eq '$($User.UserName)'" -Properties PasswordLastSet,Enabled) {
+                If ($CheckUser.Enabled -eq 'True') {
+                    If ([datetime]($CheckUser).PasswordLastSet -gt [datetime](get-date -Date $User.TimeCreated)) {
+                        If ($User.UserName -In $UsersFromGroup.UserName) {
+                            if ($Output) { write-host -ForegroundColor Green "User HAS changed password after latest risk detection => $($User.UserName)"  }
+                            Remove-ADGroupMember -Identity $ADGroup -Members $User.UserName -Confirm:$False
+                            [pscustomobject]@{Time = (Get-Date -Format "yyyy-MM-dd HH:mm:ss") ; Action = 'Removed' ; User = "$($User.UserName)" } | Export-Csv -Encoding UTF8 -Path ($MyInvocation.MyCommand.Name).replace('ps1','log') -Append -NoTypeInformation
+                        }
+                    }
+                    Else {
+                        If (-not ($User.UserName -In $UsersFromGroup.UserName)) {
+                            if ($Output) { Write-Host -ForegroundColor Red "User has NOT changed password after latest risk detection => $($User.UserName)" } 
+                            Add-ADGroupMember -Identity $ADGroup -Members $User.UserName
+                            [pscustomobject]@{Time = (Get-Date -Format "yyyy-MM-dd HH:mm:ss") ; Action = 'Added' ; User = "$($User.UserName)" } | Export-Csv -Encoding UTF8 -Path ($MyInvocation.MyCommand.Name).replace('ps1','log') -Append -NoTypeInformation
+                        }
+                    }
+                } 
                 Else {
-                    if ($Output) { Write-Host -ForegroundColor Red "User has NOT changed password after latest risk detection => $($User.UserName)" }
-                    $User
-                    # Log - Add/keep user to Group.
+                    If ($User.UserName -In $UsersFromGroup.UserName) {
+                        if ($Output) { Write-Host -ForegroundColor Cyan "User does not exist or is not enabled in AD => $($User.UserName)" }
+                        Remove-ADGroupMember -Identity $ADGroup -Members $User.UserName -Confirm:$False
+                        [pscustomobject]@{Time = (Get-Date -Format "yyyy-MM-dd HH:mm:ss") ; Action = 'Deleted' ; User = "$($User.UserName)" } | Export-Csv -Encoding UTF8 -Path ($MyInvocation.MyCommand.Name).replace('ps1','log') -Append -NoTypeInformation
+                    }
                 }
-            } 
+            }
             Else {
-                if ($Output) { Write-Host -ForegroundColor Cyan "User does not exist or is not enabled in AD => $($User.UserName)" }
-                # Log - User does not exist. Remove.
+                # User does not exist.
             }
         }
-        
-        # Clearing group members
-        if ($Output) { Write-Host -ForegroundColor Yellow Truncating the AD group. }
-        Get-ADGroup $($ADGroup) | Set-ADGroup -Clear member
 
-        # Adding users back
-        if ($Output) { Write-Host -ForegroundColor Yellow Populating the AD group with $NoDuplciatesBackToGroup.Count users. }
-        foreach ($User in $NoDuplciatesBackToGroup) {
-            Add-ADGroupMember -Identity $($ADGroup) -Members $($User.UserName)
-        }
     }
     Else {
-        Write-Host -ForegroundColor Red "Error - DCs unreachable or non-existing AD group!"
+        Write-Host -ForegroundColor Red "Error - Domain controllers unreachable or AD group does not exist!"
     }
 }
 
